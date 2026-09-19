17a1b7324224dbf7c3586d321334d02047811ce5
beellionaire <nabeelabiyu@gmail.com>
2026-09-19 07:03:54 +0700

    feat(views): menambahkan view share dan view composer

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -29,20 +29,24 @@
     @endfor
 
     <!-- STRUKTUR LAYOUT -->
-    <!--
+    {{--
         Digunakan untuk membangun arsitektur halaman web agar komponen seperti navbar atau sidebar tidak perlu ditulis berulang-ulang di setiap halaman.
         1. @extends('layouts.app'): Menandakan bahwa file view saat ini mewarisi (menggunakan kerangka) dari file master app.blade.php.
         2. @yield('content'): Digunakan di file master untuk menentukan di mana konten dari file child [@section(...)] akan ditampilkan.
         3. @section('content') ... @endsection: Digunakan di file child untuk mengisi blok yang sudah disiapkan oleh @yield.
         4. @include('partials.navbar'): Menyisipkan keseluruhan isi file view lain ke dalam view saat ini.
-    -->
+    --}}
 
     <!-- FORM DAN KEAMANAN -->
-    <!--
+    {{--
        Laravel mewajibkan pengamanan pada form yang memodifikasi data. Directives ini mempermudah implementasinya.
        1. @csrf: Wajib diletakkan di dalam tag <form> untuk method POST, PUT, PATCH, dan DELETE guna menghasilkan input token tersembunyi yang mencegah serangan Cross-Site Request Forgery.
        2. @method('PUT'): HTML standar hanya mendukung form GET dan POST. Directive ini memalsukan method HTTP agar form bisa mengirimkan request PUT, PATCH, atau DELETE ke route Laravel Anda.
-    -->
+    --}}
+
+    @foreach($products as $product)
+        <h1>{{ $product }}</h1>
+    @endforeach
 
 
 </body>
